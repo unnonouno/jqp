560fa1224921d4353f56eaff63f9e07bc799abe7
Yuya Unno <unnonouno@gmail.com>
2016-10-15 09:54:05 +0900

    Split main

--- jqp/__init__.py
+++ jqp/__init__.py
@@ -1,1 +1,20 @@
+import argparse
+import json
+import sys
+
+
 __version__ = '0.0.0'
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('cmd')
+    args = parser.parse_args()
+
+    for line in sys.stdin:
+        if line.strip() == '':
+            continue
+        js = json.loads(line)
+        out = eval(args.cmd, {'j': js})
+        json.dump(out, sys.stdout)
+        sys.stdout.write('\n')
