4ec08a37bc202d07bbd0e09e74a063644ec12169
Yuya Unno <unnonouno@gmail.com>
2016-10-15 20:01:52 +0900

    Fix test

--- tests/run_test.py
+++ tests/run_test.py
@@ -10,8 +10,8 @@ import jqp
 class RunTest(unittest.TestCase):
 
     def test1(self):
-        inputs = StringIO.StringIO('''{"name": "Taro", "age": 10}
+        inputs = StringIO('''{"name": "Taro", "age": 10}
 ''')
-        outputs = StringIO.StringIO()
+        outputs = StringIO()
         jqp.run(inputs, outputs, 'j["name"]')
         self.assertEqual(outputs.getvalue(), '"Taro"\n')
