5ac27c69a62d57aaf66adb9b6b05b08f8580946c
Yuya Unno <unnonouno@gmail.com>
2016-10-15 20:08:00 +0900

    Wrote Travis config (#9)
    * Write travis config
    * Use io in py3

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,7 +1,7 @@
 try:
-    import cStringIO as StringIO
+    from cStringIO import StringIO
 except ImportError:
-    import StringIO
+    from io import StringIO
 import unittest
 
 import jqp
@@ -10,8 +10,8 @@ import jqp
 class RunTest(unittest.TestCase):
 
     def test1(self):
-        inputs = StringIO.StringIO('''{"name": "Taro", "age": 10}
+        inputs = StringIO('''{"name": "Taro", "age": 10}
 ''')
-        outputs = StringIO.StringIO()
+        outputs = StringIO()
         jqp.run(inputs, outputs, 'j["name"]')
         self.assertEqual(outputs.getvalue(), '"Taro"\n')
