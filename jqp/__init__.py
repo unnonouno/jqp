8cc03e38e9d524acac51c657a117141f1669e6fb
Yuya Unno <unnonouno@gmail.com>
2016-10-17 23:29:26 +0900

    Show version number

--- jqp/__init__.py
+++ jqp/__init__.py
@@ -19,6 +19,9 @@ def run(in_io, out_io, cmd):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('cmd')
+    parser.add_argument(
+        '--version', action='version', version='jqp %s' % __version__,
+        help='show version and exit')
     args = parser.parse_args()
 
     run(sys.stdin, sys.stdout, args.cmd)
