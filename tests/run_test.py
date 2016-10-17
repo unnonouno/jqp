a0bedf99e7f8d490e24c51036ea0771280881c55
Yuya Unno <unnonouno@gmail.com>
2016-10-18 01:09:52 +0900

    Write error handling (#19)

--- tests/run_test.py
+++ tests/run_test.py
@@ -15,3 +15,24 @@ class RunTest(unittest.TestCase):
         outputs = StringIO()
         jqp.run(inputs, outputs, 'j["name"]')
         self.assertEqual(outputs.getvalue(), '"Taro"\n')
+
+    def test_parse_error(self):
+        inputs = StringIO('invalid\n')
+        outputs = StringIO()
+        with self.assertRaises(SystemExit) as e:
+            jqp.run(inputs, outputs, 'j')
+        self.assertEqual(e.exception.code, 4)
+
+    def test_execution_error(self):
+        inputs = StringIO('1\n')
+        outputs = StringIO()
+        with self.assertRaises(SystemExit) as e:
+            jqp.run(inputs, outputs, 'invalid')
+        self.assertEqual(e.exception.code, 3)
+
+    def test_dump_error(self):
+        inputs = StringIO('1\n')
+        outputs = StringIO()
+        with self.assertRaises(SystemExit) as e:
+            jqp.run(inputs, outputs, 'lambda: 0')
+        self.assertEqual(e.exception.code, 3)
