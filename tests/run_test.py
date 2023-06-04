0934163d6537b43f5439b4f3b39b886b137e1998
Yuya Unno <unnonouno@gmail.com>
2023-06-05 01:12:08 +0900

    Use modern pytest style (#46)

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,110 +1,117 @@
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from io import StringIO
-import unittest
+from io import StringIO
 
 import jqp
+import pytest
 
 
-class RunTest(unittest.TestCase):
-    def test1(self) -> None:
-        inputs = StringIO(
-            """{"name": "Taro", "age": 10}
+def test1() -> None:
+    inputs = StringIO(
+        """{"name": "Taro", "age": 10}
 """
-        )
-        outputs = StringIO()
-        jqp.run(inputs, outputs, 'j["name"]')
-        self.assertEqual(outputs.getvalue(), '"Taro"\n')
+    )
+    outputs = StringIO()
+    jqp.run(inputs, outputs, 'j["name"]')
+    assert outputs.getvalue() == '"Taro"\n'
 
-    def test_import(self) -> None:
-        inputs = StringIO(
-            """{"name": "Taro", "age": 10}
+
+def test_import() -> None:
+    inputs = StringIO(
+        """{"name": "Taro", "age": 10}
 """
-        )
-        outputs = StringIO()
-        jqp.run(inputs, outputs, 're.sub("a", "A", j["name"])', imports=["re"])
-        self.assertEqual(outputs.getvalue(), '"TAro"\n')
+    )
+    outputs = StringIO()
+    jqp.run(inputs, outputs, 're.sub("a", "A", j["name"])', imports=["re"])
+    assert outputs.getvalue() == '"TAro"\n'
 
-    def test_sort_keys(self) -> None:
-        # This command ignores input
-        inputs = StringIO(
-            """1
+
+def test_sort_keys() -> None:
+    # This command ignores input
+    inputs = StringIO(
+        """1
 """
-        )
-        outputs = StringIO()
-        jqp.run(inputs, outputs, '{"a": 0, "b": 0, "c": 0}', sort_keys=True)
-        self.assertEqual(outputs.getvalue(), '{"a": 0, "b": 0, "c": 0}\n')
+    )
+    outputs = StringIO()
+    jqp.run(inputs, outputs, '{"a": 0, "b": 0, "c": 0}', sort_keys=True)
+    assert outputs.getvalue() == '{"a": 0, "b": 0, "c": 0}\n'
 
-    def test_raw_input(self) -> None:
-        inputs = StringIO(" a \n")
-        outputs = StringIO()
-        jqp.run(inputs, outputs, "j", raw_input_mode=True)
-        self.assertEqual(outputs.getvalue(), '" a "\n')
 
-    def test_raw_output(self) -> None:
-        # This command ignores input
-        inputs = StringIO(
-            """1
+def test_raw_input() -> None:
+    inputs = StringIO(" a \n")
+    outputs = StringIO()
+    jqp.run(inputs, outputs, "j", raw_input_mode=True)
+    assert outputs.getvalue() == '" a "\n'
+
+
+def test_raw_output() -> None:
+    # This command ignores input
+    inputs = StringIO(
+        """1
 """
-        )
-        outputs = StringIO()
-        jqp.run(inputs, outputs, '"a"', raw_output=True)
-        self.assertEqual(outputs.getvalue(), "a\n")
+    )
+    outputs = StringIO()
+    jqp.run(inputs, outputs, '"a"', raw_output=True)
+    assert outputs.getvalue() == "a\n"
 
-    def test_join_output(self) -> None:
-        inputs = StringIO(
-            """1
+
+def test_join_output() -> None:
+    inputs = StringIO(
+        """1
 2"""
-        )
-        outputs = StringIO()
-        jqp.run(inputs, outputs, "j", join_output=True)
-        self.assertEqual(outputs.getvalue(), "12")
+    )
+    outputs = StringIO()
+    jqp.run(inputs, outputs, "j", join_output=True)
+    assert outputs.getvalue() == "12"
 
-    def test_ascii(self) -> None:
-        # This command ignores input
-        inputs = StringIO(
-            """1
+
+def test_ascii() -> None:
+    # This command ignores input
+    inputs = StringIO(
+        """1
 """
-        )
-        outputs = StringIO()
-        # This is a Japanese character
-        jqp.run(inputs, outputs, '"\u3042"', ascii_output=True)
-        self.assertEqual(outputs.getvalue(), '"\\u3042"\n')
+    )
+    outputs = StringIO()
+    # This is a Japanese character
+    jqp.run(inputs, outputs, '"\u3042"', ascii_output=True)
+    assert outputs.getvalue() == '"\\u3042"\n'
 
-    def test_indent(self) -> None:
-        inputs = StringIO(
-            """[1]
+
+def test_indent() -> None:
+    inputs = StringIO(
+        """[1]
 """
-        )
-        outputs = StringIO()
-        jqp.run(inputs, outputs, "j", indent=4)
-        self.assertEqual(outputs.getvalue(), "[\n    1\n]\n")
+    )
+    outputs = StringIO()
+    jqp.run(inputs, outputs, "j", indent=4)
+    assert outputs.getvalue() == "[\n    1\n]\n"
 
-    def test_parse_error(self) -> None:
-        inputs = StringIO("invalid\n")
-        outputs = StringIO()
-        with self.assertRaises(SystemExit) as e:
-            jqp.run(inputs, outputs, "j")
-        self.assertEqual(e.exception.code, 4)
 
-    def test_execution_error(self) -> None:
-        inputs = StringIO("1\n")
-        outputs = StringIO()
-        with self.assertRaises(SystemExit) as e:
-            jqp.run(inputs, outputs, "invalid")
-        self.assertEqual(e.exception.code, 3)
+def test_parse_error() -> None:
+    inputs = StringIO("invalid\n")
+    outputs = StringIO()
+    with pytest.raises(SystemExit) as e:
+        jqp.run(inputs, outputs, "j")
+    assert e.value.code == 4
 
-    def test_dump_error(self) -> None:
-        inputs = StringIO("1\n")
-        outputs = StringIO()
-        with self.assertRaises(SystemExit) as e:
-            jqp.run(inputs, outputs, "lambda: 0")
-        self.assertEqual(e.exception.code, 3)
 
-    def test_import_error(self) -> None:
-        inputs = StringIO("1\n")
-        outputs = StringIO()
-        with self.assertRaises(SystemExit) as e:
-            jqp.run(inputs, outputs, "j", imports=["unknown_module"])
-        self.assertEqual(e.exception.code, 5)
+def test_execution_error() -> None:
+    inputs = StringIO("1\n")
+    outputs = StringIO()
+    with pytest.raises(SystemExit) as e:
+        jqp.run(inputs, outputs, "invalid")
+    assert e.value.code == 3
+
+
+def test_dump_error() -> None:
+    inputs = StringIO("1\n")
+    outputs = StringIO()
+    with pytest.raises(SystemExit) as e:
+        jqp.run(inputs, outputs, "lambda: 0")
+    assert e.value.code == 3
+
+
+def test_import_error() -> None:
+    inputs = StringIO("1\n")
+    outputs = StringIO()
+    with pytest.raises(SystemExit) as e:
+        jqp.run(inputs, outputs, "j", imports=["unknown_module"])
+    assert e.value.code == 5
